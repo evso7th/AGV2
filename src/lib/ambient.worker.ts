5350e04c19120dea14533d142f6c41931cbcb146
Eugene Somov <evso7th@gmail.com>
2025-10-10 14:20:07 +0000

    делай

--- src/lib/ambient.worker.ts
+++ src/lib/ambient.worker.ts
@@ -406,6 +406,12 @@ const Scheduler = {
 
         score.drums = Composer.generateDrums(this.barCount, density);
         
+        console.log(`[WORKER] Generated Score:`, {
+            bass: score.bass?.map(n => n.midi),
+            melody: score.melody?.map(n => n.midi),
+            accompaniment: score.accompaniment?.map(n => n.midi),
+        });
+        
         self.postMessage({ type: 'score', score, time: this.barDuration });
 
         const currentTime = this.barCount * this.barDuration;
