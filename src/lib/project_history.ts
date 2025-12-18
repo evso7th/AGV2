d3cbec2d3bb78bf52ccb3033cfb02bc13a9f3e9e
Eugene Somov <evso7th@gmail.com>
2025-12-18 14:02:12 +0000

    запиши в историю проекта - Rolling back to version 90be6b0 на начало изд

--- src/lib/project_history.ts
+++ src/lib/project_history.ts
@@ -3,3 +3,5 @@
 // откат на правильную версию accompanementSynthManager - Rolling back to version 538d34f
 // откат на правильную версию accompanementSynthManager - Rolling back to version 538d34f
 // Действие: Модификация FractalMusicEngine для генерации примитивной мелодии.
+// Rolling back to version 90be6b0 на начало издевательства прототайпера над мелодией
+// Rolling back to version 90be6b0 на начало издевательства прототайпера над мелодией
