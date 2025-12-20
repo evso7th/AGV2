f3a505948988354fb3731a0c7424738b15098677
Eugene Somov <evso7th@gmail.com>
2025-12-20 11:43:50 +0000

    запиши в историю проекта - откат на шаг назад, до выполнения Модифициров

--- src/lib/project_history.ts
+++ src/lib/project_history.ts
@@ -1,6 +1,6 @@
 // эксперимент с макронодес отменен. неоправданно сложно.
 // собрана минимально рабочая архитектура синтеза на нативных узлах web audio
-// Действие: Создание MelodySynthManager по образу и подобию AccmpanementSyntmanager на полностью нативной архитектуре web audio api. только узлы и аудиограф
+// Действие: Создание MelodySynthManager по образу и подобию AccmpanementSyntmanager на полностью нативной архитектуре web audio. только узлы и аудиограф
 // модифицирован вручную расчет lfo для более аутентичных тембров
 // откат на правильную версию accompanementSynthManager - Rolling back to version 538d34f
 // откат на правильную версию accompanementSynthManager - Rolling back to version 538d34f
@@ -10,3 +10,4 @@
 // Rolling back to version d3cbec2 опять начинаем внедрять мелодию. третий раз
 // откат. Rolling back to version de9caa8
 // откат на точку начала восстановления harmony. Rolling back to version 6b1fdd8
+// откат на шаг назад, до выполнения Модифицировать src/lib/fractal-music-engine.ts. Причина: сломал мелодию.
