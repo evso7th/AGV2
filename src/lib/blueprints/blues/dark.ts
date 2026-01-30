9f7e42c42a57fe863f15f00a19715bf045e4f283
Eugene Somov <evso7th@gmail.com>
2026-01-30 19:15:42 +0000

    Интро раздели на 3 части по 4 такта. Аутро тоже 3 части по 4 такта. Перс

--- src/lib/blueprints/blues/dark.ts
+++ src/lib/blueprints/blues/dark.ts
@@ -1,3 +1,4 @@
+
 import type { MusicBlueprint } from '@/types/music';
 
 export const DarkBluesBlueprint: MusicBlueprint = {
@@ -15,70 +16,119 @@ export const DarkBluesBlueprint: MusicBlueprint = {
     structure: {
         totalDuration: { preferredBars: 120 }, // 10 loops of 12 bars
         parts: [
+            // --- INTRO (12 bars total) ---
             {
-                id: 'INTRO', name: 'The Summoning', duration: { percent: 25 },
-                layers: { drums: true, accompaniment: true },
+                id: 'INTRO_1', name: 'The Summoning', duration: { percent: 3 }, // ~4 bars
+                layers: { drums: true, accompaniment: true, sfx: true },
                 instrumentation: {
-                    accompaniment: {
-                        strategy: 'weighted',
-                        v1Options: [{ name: 'organ_soft_jazz', weight: 1.0 }],
-                        v2Options: [{ name: 'organ_soft_jazz', weight: 1.0 }]
-                    },
+                    accompaniment: { strategy: 'weighted', v1Options: [{ name: 'organ_soft_jazz', weight: 1.0 }], v2Options: [{ name: 'organ_soft_jazz', weight: 1.0 }] },
                 },
                 instrumentRules: {
                     drums: { pattern: 'composer', kitName: 'blues_dark', density: { min: 0.1, max: 0.2 }, useSnare: false, rareKick: true },
-                    accompaniment: { register: { preferred: 'low' }, density: { min: 0.1, max: 0.3 } },
+                    accompaniment: { register: { preferred: 'low' }, density: { min: 0.1, max: 0.2 } },
                     melody: { source: 'harmony_top_note' }
                 },
-                bundles: [{ id: 'BLUES_DARK_INTRO_BUNDLE', name: 'Ritual Start', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                bundles: [{ id: 'DARK_INTRO_1_BUNDLE', name: 'Ritual Start', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
                 outroFill: null,
             },
             {
-                id: 'MAIN', name: 'The Chant', duration: { percent: 50 },
-                layers: { bass: true, drums: true, accompaniment: true, melody: true },
+                id: 'INTRO_2', name: 'Bass Drone', duration: { percent: 3 }, // ~4 bars
+                layers: { drums: true, bass: true, accompaniment: true, sfx: true },
                 instrumentation: {
-                    accompaniment: {
-                        strategy: 'weighted',
-                        v1Options: [{ name: 'organ_soft_jazz', weight: 1.0 }],
-                        v2Options: [{ name: 'organ_soft_jazz', weight: 1.0 }]
-                    },
-                    melody: {
-                        strategy: 'weighted',
-                        v1Options: [{ name: 'blackAcoustic', weight: 1.0 }],
-                        v2Options: [{ name: 'blackAcoustic', weight: 1.0 }]
-                    }
+                    accompaniment: { strategy: 'weighted', v1Options: [{ name: 'organ_soft_jazz', weight: 1.0 }], v2Options: [{ name: 'organ_soft_jazz', weight: 1.0 }] },
+                    bass: { strategy: 'weighted', v1Options: [{ name: 'ambientDrone', weight: 1.0 }], v2Options: [{ name: 'bass_ambient_dark', weight: 1.0 }] }
                 },
                 instrumentRules: {
-                    drums: { pattern: 'composer', kitName: 'blues_dark', density: { min: 0.4, max: 0.6 }, useSnare: true, kickVolume: 1.2, ride: { enabled: true, probability: 0.1 } },
+                    drums: { density: { min: 0.15, max: 0.25 } },
                     bass: { techniques: [{ value: 'drone', weight: 1.0 }] },
-                    melody: {
-                        source: 'blues_solo',
-                        density: { min: 0.3, max: 0.5 },
-                        register: { preferred: 'low' },
-                        soloPlan: "S06",
-                        presetModifiers: { reverbMix: 0.6, delay: { on: true, time: 0.5, fb: 0.4, hc: 2000, mix: 0.3 } } // FX for "ghost" guitar
-                    }
-                },
-                bundles: [{ id: 'BLUES_DARK_MAIN_BUNDLE', name: 'Main Chant', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
-                outroFill: { type: 'roll', duration: 1, parameters: { instrument: 'ride' } },
-            },
-            {
-                id: 'OUTRO', name: 'Fading Echoes', duration: { percent: 25 },
-                layers: { drums: true, accompaniment: true, sfx: true },
-                 instrumentation: {
-                    accompaniment: {
-                        strategy: 'weighted',
-                        v1Options: [{ name: 'organ_soft_jazz', weight: 1.0 }],
-                        v2Options: [{ name: 'organ_soft_jazz', weight: 1.0 }]
-                    }
-                },
-                instrumentRules: {
-                    drums: { pattern: 'composer', kitName: 'blues_dark', density: { min: 0.1, max: 0.3 }, useSnare: false, ride: { enabled: true, probability: 0.05 } },
                     melody: { source: 'harmony_top_note' }
                 },
-                bundles: [{ id: 'BLUES_DARK_OUTRO_BUNDLE', name: 'Fade Out', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                bundles: [{ id: 'DARK_INTRO_2_BUNDLE', name: 'Drone Entry', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
                 outroFill: null,
-            }
+            },
+            {
+                id: 'INTRO_3', name: 'Ghost Guitar', duration: { percent: 4 }, // ~4 bars
+                layers: { drums: true, bass: true, accompaniment: true, melody: true, sfx: true },
+                instrumentation: {
+                    melody: { strategy: 'weighted', v1Options: [{ name: 'blackAcoustic', weight: 1.0 }], v2Options: [{ name: 'blackAcoustic', weight: 1.0 }] }
+                },
+                instrumentRules: {
+                    drums: { density: { min: 0.2, max: 0.3 } },
+                    melody: { source: 'motif', density: { min: 0.1, max: 0.2 }, presetModifiers: { reverbMix: 0.7, delay: { on: true, time: 0.6, fb: 0.5, hc: 1800, mix: 0.4 } } }
+                },
+                bundles: [{ id: 'DARK_INTRO_3_BUNDLE', name: 'First Motif', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: { type: 'roll', duration: 1, parameters: { instrument: 'tom_low', density: 0.5 } },
+            },
+            // --- MAIN (96 bars total) ---
+            {
+                id: 'MAIN_1', name: 'The Chant', duration: { percent: 27 }, // ~32 bars
+                layers: { bass: true, drums: true, accompaniment: true, melody: true, sfx: true },
+                instrumentation: {
+                    melody: { strategy: 'weighted', v1Options: [{ name: 'blackAcoustic', weight: 1.0 }], v2Options: [{ name: 'blackAcoustic', weight: 1.0 }] }
+                },
+                instrumentRules: {
+                    drums: { pattern: 'composer', kitName: 'blues_dark', density: { min: 0.4, max: 0.6 }, useSnare: true, kickVolume: 1.2 },
+                    bass: { techniques: [{ value: 'riff', weight: 1.0 }] },
+                    melody: { source: 'blues_solo', density: { min: 0.3, max: 0.5 }, register: { preferred: 'low' }, soloPlan: 'S06' }
+                },
+                bundles: [{ id: 'BLUES_DARK_MAIN_A_BUNDLE', name: 'Main Riff', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: null,
+            },
+            {
+                id: 'MAIN_2', name: 'Ritual Peak', duration: { percent: 27 }, // ~32 bars
+                layers: { bass: true, drums: true, accompaniment: true, melody: true, sfx: true },
+                instrumentation: {
+                    melody: { strategy: 'weighted', v1Options: [{ name: 'blackAcoustic', weight: 1.0 }], v2Options: [{ name: 'blackAcoustic', weight: 1.0 }] }
+                },
+                instrumentRules: {
+                    drums: { density: { min: 0.5, max: 0.7 }, ride: { enabled: true, probability: 0.2 } },
+                    melody: { density: { min: 0.4, max: 0.6 }, soloPlan: 'S07' }
+                },
+                bundles: [{ id: 'BLUES_DARK_MAIN_B_BUNDLE', name: 'Intensity', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: { type: 'roll', duration: 2, parameters: { instrument: 'ride' } },
+            },
+            {
+                id: 'MAIN_3', name: 'Waning Chant', duration: { percent: 26 }, // ~32 bars
+                layers: { bass: true, drums: true, accompaniment: true, melody: true, sfx: true },
+                 instrumentation: {
+                    melody: { strategy: 'weighted', v1Options: [{ name: 'blackAcoustic', weight: 1.0 }], v2Options: [{ name: 'blackAcoustic', weight: 1.0 }] }
+                },
+                instrumentRules: {
+                    drums: { density: { min: 0.4, max: 0.6 } },
+                    melody: { density: { min: 0.3, max: 0.5 }, soloPlan: 'S08' }
+                },
+                bundles: [{ id: 'BLUES_DARK_MAIN_C_BUNDLE', name: 'Waning', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: null,
+            },
+            // --- OUTRO (12 bars total) ---
+            {
+                id: 'OUTRO_1', name: 'Fading Embers', duration: { percent: 3 }, // ~4 bars
+                layers: { bass: true, drums: true, accompaniment: true, sfx: true },
+                instrumentRules: {
+                    drums: { density: { min: 0.2, max: 0.4 }, ride: { enabled: true, probability: 0.05 } },
+                    bass: { techniques: [{ value: 'long_notes', weight: 1.0 }] }
+                },
+                bundles: [{ id: 'BLUES_DARK_OUTRO_1', name: 'Embers', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: null,
+            },
+            {
+                id: 'OUTRO_2', name: 'Cold Ash', duration: { percent: 3 }, // ~4 bars
+                layers: { accompaniment: true, sfx: true },
+                 instrumentRules: {
+                    accompaniment: { density: { min: 0.1, max: 0.2 } }
+                 },
+                bundles: [{ id: 'BLUES_DARK_OUTRO_2', name: 'Ashes', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: null,
+            },
+            {
+                id: 'OUTRO_3', name: 'Silence', duration: { percent: 4 }, // ~4 bars
+                layers: { sfx: true },
+                instrumentRules: {
+                    sfx: { eventProbability: 0.1 }
+                },
+                bundles: [{ id: 'BLUES_DARK_OUTRO_3', name: 'Final Echo', duration: { percent: 100 }, characteristics: {}, phrases: {} }],
+                outroFill: null,
+            },
         ]
     },
     mutations: {},
