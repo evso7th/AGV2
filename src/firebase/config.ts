9ba9b752a8fdb0683a983f25a08c6410bc707b50
Eugene Somov <evso7th@gmail.com>
2026-02-07 16:22:12 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "aura-groove.firebaseapp.com",
-  projectId: "aura-groove",
-  storageBucket: "aura-groove.appspot.com",
-  messagingSenderId: "placeholder-id",
-  appId: "placeholder-app-id"
+  "projectId": "studio-5974577704-204a1",
+  "appId": "1:140690095294:web:68d0e3a8e0b56c2af072ef",
+  "apiKey": "AIzaSyC4nA1UuFZ6znv45LZXqsQHwDgGAmvrBh4",
+  "authDomain": "studio-5974577704-204a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "140690095294"
 };
