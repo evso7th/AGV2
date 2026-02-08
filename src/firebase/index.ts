df1055f4c3bda7c5061009975fcc82f4e751e594
Eugene Somov <evso7th@gmail.com>
2026-02-08 06:05:19 +0000

    Все еще не получается запись генофонда от пользователя. это связанные со

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, initializeFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -33,10 +33,16 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  // #ЗАЧЕМ: Исправление сетевых ошибок WebChannel в среде разработки.
+  // #ЧТО: Использование принудительного Long Polling.
+  const firestore = initializeFirestore(firebaseApp, {
+    experimentalForceLongPolling: true,
+  });
+
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore
   };
 }
 
