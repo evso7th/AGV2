c770f7dae6f51d26662764d34cb21d1ef119b185
Eugene Somov <evso7th@gmail.com>
2026-06-02 14:04:14 +0000

    ОК, молодец, точно следуешь моим инструкциям! премия 50$. надо привести

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
 const nextConfig = {
   output: 'export',
   typescript: {
@@ -23,4 +30,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
